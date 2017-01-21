efe4c1cb1ab58fa3a9b45cff3ca0151f48f4fbc9
EugeneTymoshenko <jack.tymoshenko@gmail.com>
2017-01-22 00:26:14 +0200

    implemented 3 methods to execute program

--- lab1.rb
+++ lab1.rb
@@ -1,1 +1,6 @@
-p m_rand = Array.new(4){Array.new(4){rand} }
+p i = Array.new(4){ Array.new(4) { rand(-10..10) } }.flatten.select { |i| i < 0 }.inject(:+)
+p i = Array.new(4){ Array.new(4) { rand(-10..10) } }
+s = 0
+p i.flatten.each{ |i| s += i if i < 0 }
+p s
+p i = Array.new(4) { Array.new(4) { rand(-10..10) } }.flatten.delete_if {|i| i > 0 }.inject(:+)
