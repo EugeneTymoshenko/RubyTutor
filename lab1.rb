3af946eeec47774d0955a3f60ee70fd4d2bcc3da
EugeneTymoshenko <jack.tymoshenko@gmail.com>
2017-01-22 14:23:24 +0200

    Five methods done

--- lab1.rb
+++ lab1.rb
@@ -1,6 +1,20 @@
+# select method
 p i = Array.new(4){ Array.new(4) { rand(-10..10) } }.flatten.select { |i| i < 0 }.inject(:+)
-p i = Array.new(4){ Array.new(4) { rand(-10..10) } }
+# each method
+i = Array.new(4){ Array.new(4) { rand(-10..10) } }
 s = 0
-p i.flatten.each{ |i| s += i if i < 0 }
+i.flatten.each{ |i| s += i if i < 0 }
 p s
+# delete_if method
 p i = Array.new(4) { Array.new(4) { rand(-10..10) } }.flatten.delete_if {|i| i > 0 }.inject(:+)
+# collect method
+i = Array.new(4) { Array.new(4) { rand(-10..10) } }
+s = 0
+i.flatten.collect {|i| s += i if i < 0 }
+p s
+# map! method
+i = Array.new(4) { Array.new(4) { rand(-10..10) } }
+s = 0
+i.flatten.map! { |i| s += i if i < 0 }
+p s
+
