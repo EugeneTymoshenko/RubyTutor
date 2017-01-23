798280e356ec042cf310d474811359a3c5f05393
EugeneTymoshenko <jack.tymoshenko@gmail.com>
2017-01-23 15:45:48 +0200

    implemented through the 5 methods

--- lab1.rb
+++ lab1.rb
@@ -10,7 +10,7 @@ p i = Array.new(4) { Array.new(4) { rand(-10..10) } }.flatten.delete_if {|i| i >
 # collect method
 i = Array.new(4) { Array.new(4) { rand(-10..10) } }
 s = 0
-i.flatten.collect {|i| s += i if i < 0 }
+i.flatten.collect { |i| s += i if i < 0 }
 p s
 # map! method
 i = Array.new(4) { Array.new(4) { rand(-10..10) } }
