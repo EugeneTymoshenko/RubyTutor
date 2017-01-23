1bc6ea103d2b38080000c6277c77d4bdbbcd8aab
EugeneTymoshenko <jack.tymoshenko@gmail.com>
2017-01-23 18:32:16 +0200

    Completed 2 tasks with 3 methods for each task

--- lab2.rb
+++ lab2.rb
@@ -1,6 +1,12 @@
-h = { 'yesetas' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', :yesss => :fg,
+h = { '1yesetas' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', :yesss => :fg,
 try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014 }
-yes = /yes/
 #size method
-p h.select {|h| h =~ /yes/}.size
-p h.select {|h| h =~ /yes/}.length
+p h.select {|k| k =~ /\Ayes/}.size
+p h.select {|k| k =~ /\Ayes/}.length
+p h.select {|k| k =~ /\Ayes/}.count
+
+b = {'yesetas' => 23, 'b' => 'travel', 'yesterdayas' => 34, 5 => '234', :yesss => :fg,
+try: 30, key: 'some value', 'yesterday1as' => 34, 'yesteryearasd' => 2014 }
+p b.select { |k| k =~ /1as\z/}.size
+p b.select { |k| k =~ /1as\z/}.length
+p b.select { |k| k =~ /1as\z/}.count
